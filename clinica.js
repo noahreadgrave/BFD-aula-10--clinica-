be6eed6d10e59d986c47c696fafc6562d4afcd72
noahreadgrave <noah.redgrave.nr@gmail.com>
2025-09-05 14:40:02 -0300

    Add files via upload

---
+++ clinica.js
@@ -0,0 +1,22 @@
+const obj = {
+
+    nome: "Pequenininho",
+    especie: "gato",
+    raça: "viralata",
+    idade: 3
+    
+};
+ 
+console.log(`o nome do meu paciente é ${obj.nome}.`);
+
+const obj2 = {
+
+    nome: "frajola",
+    especie: "gato",
+    raça: "frajola",
+    idade: 1
+
+
+};
+
+console.log(`já o meu outro paciente se chama ${obj2.nome}.`);
