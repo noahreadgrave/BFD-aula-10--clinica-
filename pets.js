ab6476d5ecc3e22203a14b3509b5579e78d89510
noahreadgrave <noah.redgrave.nr@gmail.com>
2025-09-12 15:46:15 -0300

    Add files via upload

---
+++ pets.js
@@ -0,0 +1,32 @@
+const pet1 = {
+
+nome: "pequenininho",
+idade: 2,
+especie: "gato",
+raça: "vira lata",
+
+}
+
+
+const pet2 = {
+nome: "frajola",
+idade: 1,
+especie: "gato",
+raça: "vira lata",
+}
+
+
+const pet3 = {
+nome: "klaiton",
+idade: 5,
+especie: "gato",
+raça: "vira lata",
+}
+
+
+const pet4 = {
+nome: "thomas",
+idade: 4,
+especie: "gato",
+raça: "vira lata",
+}
