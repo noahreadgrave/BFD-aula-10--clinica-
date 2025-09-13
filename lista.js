6406665f25f8d035af26205f58b3eaa7885a2149
noahreadgrave <noah.redgrave.nr@gmail.com>
2025-09-13 11:21:40 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,25 @@
+// Q1.A) Crie uma variável constante chamada nome, armazenando uma string com seu nome
+
+
+// Q1.B) Imprima na tela a frase "Meu nome é SEU NOME. Este nome tem NUMERO DE LETRAS letras", trocando SEU NOME e NUMERO DE LETRAS pelos valores correspondentes
+
+
+// Q1.C) Crie uma função umaLetraDeVez(palavra), que imprime todas as letras da palavra uma de cada vez
+
+// Q1.D) Chame a função umaLetraDeVez passando como argumento a variável nome//
+
+function nomedizer() {
+    const nome = "noah";
+    console.log(`meu nome é ${nome}`);
+    console.log(`este nome tem ${nome.length} letras.`);
+}
+
+
+nomedizer();
+
+function umaLetraDeVez (palavra){
+
+
+    
+}
+
